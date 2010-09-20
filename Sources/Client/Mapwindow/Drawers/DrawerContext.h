63e9bf4a165251e6a86830bc4748958855ddc904
Martin Schouwenburg <m.l.schouwenburg@utwente.nl>
2010-09-20 14:05:30 +0000

    ok
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@517 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/Client/Mapwindow/Drawers/DrawerContext.h
+++ Sources/Client/Mapwindow/Drawers/DrawerContext.h
@@ -8,37 +8,14 @@
 
 class MapCompositionDoc;
 
-namespace ILWIS {
+namespace ILWIS { 
 	class RootDrawer;
 	class _export DrawerContext {
 	public:
 		~DrawerContext();
-		DrawerContext(MapCompositionDoc *, RootDrawer *dr);
-		CoordBounds getCoordBoundsView() const { return cbView; }
-		CoordBounds getCoordBoundsZoom() const; 
-		CoordBounds getMapCoordBounds() const;
-		CoordSystem getCoordinateSystem() const { return cs;}
-		RowCol getViewPort() const { return pixArea; }
-		void setCoordinateSystem(const CoordSystem& _cs, bool overrule) ;
-		void setCoordBoundsView(const CoordBounds& _cb, bool overrule); 
-		void setCoordBoundsZoom(const CoordBounds& _cb);
-		void setCoordBoundsMap(const CoordBounds& cb);
-		void setZoom(const CRect& rct);
-		void setViewPort(const RowCol& rc);
-		Coord screenToWorld(const RowCol& rc);
-		RowCol worldToScreen(const Coord& crd);
-		double getAspectRatio() const;
+		DrawerContext(MapCompositionDoc *);
 		bool initOpenGL(CDC *dc);
 		void clear();
-		RootDrawer *getRootDrawer() const;
-		void setRootDrawer(RootDrawer *dr);
-		void set3D(bool yeno);
-		bool is3D() const;
-		void setViewPoint(const Coord& c);
-		void setEyePoint(const Coord& c);
-		Coord getViewPoint() const;
-		Coord getEyePoint() const;
-	    double getFakeZ() const;
 		MapCompositionDoc * getDocument() const;
 		GLint getMaxTextureSize() const { return maxTextureSize; };
 
@@ -49,21 +26,6 @@ namespace ILWIS {
 		bool fUrgentRequestWaiting; // intentionally public!! used in volatile* to abort texture generation in thread
 
 	private:
-		void setProjection(const CoordBounds& cb);
-		void setEyePoint();
-		void modifyCBZoomView(double dv, double dz, double f);
-
-		CoordBounds cbView;
-		CoordBounds cbZoom;
-		CoordBounds cbMap;
-		CoordSystem cs;
-		RowCol pixArea;
-		double aspectRatio;
-		RootDrawer *rootDrawer;
-		bool threeD;
-		Coordinate eyePoint;
-		Coordinate viewPoint;
-		double fakeZ;
 		MapCompositionDoc *doc;
 		GLint maxTextureSize;
 
