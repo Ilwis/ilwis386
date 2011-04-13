a74d948b892319ceee4beb87901ff307560f32b8
Martin Schouwenburg <m.l.schouwenburg@utwente.nl>
2011-04-13 08:02:19 +0000

    adapted moduleinfo method
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@942 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/PointApplicationsUI/apps.cpp
+++ Sources/PointApplicationsUI/apps.cpp
@@ -55,34 +55,34 @@ Last change:  WK   17 Sep 98    2:14 pm
 #include "Client\ilwis.h"
 #include "Headers\Hs\Applications.hs"
 
-extern "C" _export InfoUIVector* getApplicationInfoUI() {
+extern "C" _export InfoUIVector* getCommandInfoUI(ILWIS::Module *module) {
 	InfoUIVector *infosui = new InfoUIVector();
 	String ilwDir = IlwWinApp()->Context()->sIlwDir();
 	String hyrdoDemBase = ilwDir + "Scripts\\Hydro-DEM\\";
 
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("attribpnt","",Cmdattribpnt,SMENUattribpnt,SMENUSattribpnt,"ExePnt16IcoL",".mpp",4702,SMENUDattribpnt));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("maskpnt","",Cmdmaskpnt,SMENUmaskpnt,SMENUSmaskpnt,"ExePnt16IcoL",".mpp",4782,SMENUDmaskpnt));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("subpnt","",Cmdsubpnt,SMENUsubpnt,SMENUSsubpnt,"ExePnt16IcoL",".mpp",4752,SMENUDsubpnt));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("gluepnt","",Cmdgluepnt,SMENUgluepnt,SMENUSgluepnt,"ExePnt16IcoL",".mpp",4762,SMENUDgluepnt));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("transfpnt","",Cmdtransfpnt,SMENUtransfpnt,SMENUStransfpnt,"ExePnt16IcoL",".mpp.csy",4742,SMENUDtransfpnt));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("raspnt","",Cmdraspnt,SMENUraspnt,SMENUSraspnt,"ExePnt16IcoL",".mpr",4712,SMENUDraspnt));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("polpnt","",Cmdpolpnt,SMENUpolpnt,SMENUSpolpnt,"ExePnt16IcoL",".mpa",4732,SMENUDpolpnt));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("segpnt","",Cmdsegpnt,SMENUsegpnt,SMENUSsegpnt,"ExePnt16IcoL",".mps",4724,SMENUDsegpnt));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("tblpnt","",Cmdtblpnt,SMENUtblpnt,SMENUStblpnt,"ExePnt16IcoL",".tbt",4792,SMENUDtblpnt));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("attribpnt","",Cmdattribpnt,SMENUattribpnt,SMENUSattribpnt,"ExePnt16IcoL",".mpp",4702,SMENUDattribpnt));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("maskpnt","",Cmdmaskpnt,SMENUmaskpnt,SMENUSmaskpnt,"ExePnt16IcoL",".mpp",4782,SMENUDmaskpnt));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("subpnt","",Cmdsubpnt,SMENUsubpnt,SMENUSsubpnt,"ExePnt16IcoL",".mpp",4752,SMENUDsubpnt));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("gluepnt","",Cmdgluepnt,SMENUgluepnt,SMENUSgluepnt,"ExePnt16IcoL",".mpp",4762,SMENUDgluepnt));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("transfpnt","",Cmdtransfpnt,SMENUtransfpnt,SMENUStransfpnt,"ExePnt16IcoL",".mpp.csy",4742,SMENUDtransfpnt));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("raspnt","",Cmdraspnt,SMENUraspnt,SMENUSraspnt,"ExePnt16IcoL",".mpr",4712,SMENUDraspnt));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("polpnt","",Cmdpolpnt,SMENUpolpnt,SMENUSpolpnt,"ExePnt16IcoL",".mpa",4732,SMENUDpolpnt));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("segpnt","",Cmdsegpnt,SMENUsegpnt,SMENUSsegpnt,"ExePnt16IcoL",".mps",4724,SMENUDsegpnt));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("tblpnt","",Cmdtblpnt,SMENUtblpnt,SMENUStblpnt,"ExePnt16IcoL",".tbt",4792,SMENUDtblpnt));
 
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("intersectpnt","",Cmdpointmapintersect,SMENUpntintersection,SMENUSpntintersection,"ExePnt16IcoL",".mpp.mps.mpa",0,SMENUDpntintersection));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("unionpnt","",Cmdpointmapunion,SMENUpntunion,SMENUSpntunion,"ExePnt16IcoL",".mpp.mps",0,SMENUDpntunion));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("diffpnt","",Cmdpointmapdifference,SMENUpntdifference,SMENUSpntdifference,"ExePnt16IcoL",".mpp",0,SMENUDpntdifference));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("symdiffpnt","",Cmdpointmapsymetricdifference,SMENUpntsymetricdifference,SMENUSpntsymetricdifference,"ExePnt16IcoL",".mpp.mps",0,SMENUDpntsymetricdifference));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("segmiddlepnt","",Cmdpointinsegment,SMENUpointinsegment,SMENUSpointinsegment,"ExePnt16IcoL",".mps",0,SMENUDpointinsegment));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("relatepnt","",Cmdpointmaprelate,SMENUpntrelate,SMENUSpntrelate,"ExePnt16IcoL",".mpp",0,SMENUDpntrelate));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("intersectpnt","",Cmdpointmapintersect,SMENUpntintersection,SMENUSpntintersection,"ExePnt16IcoL",".mpp.mps.mpa",0,SMENUDpntintersection));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("unionpnt","",Cmdpointmapunion,SMENUpntunion,SMENUSpntunion,"ExePnt16IcoL",".mpp.mps",0,SMENUDpntunion));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("diffpnt","",Cmdpointmapdifference,SMENUpntdifference,SMENUSpntdifference,"ExePnt16IcoL",".mpp",0,SMENUDpntdifference));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("symdiffpnt","",Cmdpointmapsymetricdifference,SMENUpntsymetricdifference,SMENUSpntsymetricdifference,"ExePnt16IcoL",".mpp.mps",0,SMENUDpntsymetricdifference));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("segmiddlepnt","",Cmdpointinsegment,SMENUpointinsegment,SMENUSpointinsegment,"ExePnt16IcoL",".mps",0,SMENUDpointinsegment));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("relatepnt","",Cmdpointmaprelate,SMENUpntrelate,SMENUSpntrelate,"ExePnt16IcoL",".mpp",0,SMENUDpntrelate));
 
 	return infosui;
 }
 
-extern "C" _export ILWIS::Module *getModuleInfo() {
-	ILWIS::Module *module = new ILWIS::Module("Pointnmap applications UI", "PointApplicationsUI.dll",ILWIS::Module::mi37,"1.0");
-	module->addMethod(ILWIS::Module::ifGetAppInfoUI, (void *)getApplicationInfoUI);  
+extern "C" _export ILWIS::Module *getModuleInfo(const FileName& fnModule) {
+	ILWIS::Module *module = new ILWIS::Module("Pointnmap applications UI", fnModule,ILWIS::Module::mi37,"1.0");
+	module->addMethod(ILWIS::Module::ifgetCommandInfoUI, (void *)getCommandInfoUI);  
 
 	return module;
 }
