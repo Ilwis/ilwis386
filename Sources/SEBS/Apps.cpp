50459a1b2ab13178f4f43700473909fb07fc3344
Martin Schouwenburg <m.l.schouwenburg@utwente.nl>
2011-04-13 08:02:43 +0000

    adapted moduleinfo method
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@945 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/SEBS/Apps.cpp
+++ Sources/SEBS/Apps.cpp
@@ -12,26 +12,26 @@
 #include "SEBS\MapETo.h"
 #include "Client\ilwis.h"
 
-InfoVector* getApplicationInfo() {
+InfoVector* getCommandInfo() {
 
 	InfoVector *infos = new InfoVector();
-	(*infos).push_back(ApplicationMap::newApplicationInfo(createMapAlbedo,"MapAlbedo"));
-	(*infos).push_back(ApplicationMap::newApplicationInfo(createMapLandSurfaceTemperature,"MapLandSurfaceTemperature"));
-	(*infos).push_back(ApplicationMap::newApplicationInfo(createMapEmissivity,"MapEmissivity"));
-	(*infos).push_back(ApplicationMap::newApplicationInfo(createMapSEBS,"MapSEBS"));
-	(*infos).push_back(ApplicationMap::newApplicationInfo(createMapSI2Radiance,"MapSI2Radiance"));
-	(*infos).push_back(ApplicationMap::newApplicationInfo(createMapSMAC,"MapSMAC"));
-	(*infos).push_back(ApplicationMap::newApplicationInfo(createMapETo,"MapETo"));
-	(*infos).push_back(ApplicationMap::newApplicationInfo(createMapRadiance2Reflectance,"MapRadiance2Reflectance"));
-	(*infos).push_back(ApplicationMap::newApplicationInfo(createMapBrightnessTemperature,"MapBrightnessTemperature"));
+	(*infos).push_back(CommandMap::newCommandInfo(createMapAlbedo,"MapAlbedo"));
+	(*infos).push_back(CommandMap::newCommandInfo(createMapLandSurfaceTemperature,"MapLandSurfaceTemperature"));
+	(*infos).push_back(CommandMap::newCommandInfo(createMapEmissivity,"MapEmissivity"));
+	(*infos).push_back(CommandMap::newCommandInfo(createMapSEBS,"MapSEBS"));
+	(*infos).push_back(CommandMap::newCommandInfo(createMapSI2Radiance,"MapSI2Radiance"));
+	(*infos).push_back(CommandMap::newCommandInfo(createMapSMAC,"MapSMAC"));
+	(*infos).push_back(CommandMap::newCommandInfo(createMapETo,"MapETo"));
+	(*infos).push_back(CommandMap::newCommandInfo(createMapRadiance2Reflectance,"MapRadiance2Reflectance"));
+	(*infos).push_back(CommandMap::newCommandInfo(createMapBrightnessTemperature,"MapBrightnessTemperature"));
 
 	return infos;
 
 }
 
-extern "C" _export ILWIS::Module *getModuleInfo() {
-	ILWIS::Module *module = new ILWIS::Module("SEBS", "IlwisSEBS.dll",ILWIS::Module::mi37,"1.0");
-	module->addMethod(ILWIS::Module::ifGetAppInfo, (void *)getApplicationInfo);  
+extern "C" _export ILWIS::Module *getModuleInfo(const FileName& fnModule) {
+	ILWIS::Module *module = new ILWIS::Module("SEBS", fnModule,ILWIS::Module::mi37,"1.0");
+	module->addMethod(ILWIS::Module::ifgetCommandInfo, (void *)getCommandInfo);  
 
 	return module;
 }
