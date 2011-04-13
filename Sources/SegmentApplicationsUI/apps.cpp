2b81a934a91d95208a0ba100aac31252fa6702be
Martin Schouwenburg <m.l.schouwenburg@utwente.nl>
2011-04-13 08:02:59 +0000

    adapted moduleinfo method
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@947 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/SegmentApplicationsUI/apps.cpp
+++ Sources/SegmentApplicationsUI/apps.cpp
@@ -69,38 +69,38 @@ Last change:  WK   17 Sep 98    2:14 pm
 #include "SegmentApplicationsUI\SegmentMapFromRasValueBndFrm.h"
 #include "Headers\Hs\Applications.hs"
 
-InfoUIVector* getApplicationInfoUI() {
+InfoUIVector* getCommandInfoUI(ILWIS::Module *module) {
 	InfoUIVector *infosui = new InfoUIVector();
 	String ilwDir = IlwWinApp()->Context()->sIlwDir();
 	
 
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("attribseg","",Cmdattribseg,SMENUattribseg,SMENUSattribseg,"ExeSeg16IcoL",".mps",4822,SMENUDattribseg));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("maskseg","",Cmdmaskseg,SMENUmaskseg,SMENUSmaskseg,"ExeSeg16IcoL",".mps",4812,SMENUDmaskseg));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("labelseg","",Cmdlabelseg,SMENUlabelseg,SMENUSlabelseg,"ExeSeg16IcoL",".mps",4832,SMENUDlabelseg));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("subseg","",Cmdsubseg,SMENUsubseg,SMENUSsubseg,"ExeSeg16IcoL",".mps",4862,SMENUDsubseg));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("glueseg","",Cmdglueseg,SMENUglueseg,SMENUSglueseg,"ExeSeg16IcoL",".mps",4882,SMENUDglueseg));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("densseg","",Cmddensseg,SMENUdensseg,SMENUSdensseg,"ExeSeg16IcoL",".mps",4872,SMENUDdensseg));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("transfseg","",Cmdtransfseg,SMENUtransfseg,SMENUStransfseg,"ExeSeg16IcoL",".mps.csy",4842,SMENUDtransfseg));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("tunnelseg","",Cmdtunnelseg,SMENUtunnelseg,SMENUStunnelseg,"ExeSeg16IcoL",".mps",4852,SMENUDtunnelseg));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("cleanseg","",Cmdcleanseg,SMENUcleanseg,SMENUScleanseg,"ExeSeg16IcoL",".mps",4802,SMENUDcleanseg));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("polseg","",Cmdpolseg,SMENUpolseg,SMENUSpolseg,"ExeSeg16IcoL",".mpa",4892,SMENUDpolseg));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("rasseg","",Cmdrasseg,SMENUrasseg,SMENUSrasseg,"ExeSeg16IcoL",".mpr",4902,SMENUDrasseg));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("attribseg","",Cmdattribseg,SMENUattribseg,SMENUSattribseg,"ExeSeg16IcoL",".mps",4822,SMENUDattribseg));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("maskseg","",Cmdmaskseg,SMENUmaskseg,SMENUSmaskseg,"ExeSeg16IcoL",".mps",4812,SMENUDmaskseg));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("labelseg","",Cmdlabelseg,SMENUlabelseg,SMENUSlabelseg,"ExeSeg16IcoL",".mps",4832,SMENUDlabelseg));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("subseg","",Cmdsubseg,SMENUsubseg,SMENUSsubseg,"ExeSeg16IcoL",".mps",4862,SMENUDsubseg));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("glueseg","",Cmdglueseg,SMENUglueseg,SMENUSglueseg,"ExeSeg16IcoL",".mps",4882,SMENUDglueseg));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("densseg","",Cmddensseg,SMENUdensseg,SMENUSdensseg,"ExeSeg16IcoL",".mps",4872,SMENUDdensseg));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("transfseg","",Cmdtransfseg,SMENUtransfseg,SMENUStransfseg,"ExeSeg16IcoL",".mps.csy",4842,SMENUDtransfseg));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("tunnelseg","",Cmdtunnelseg,SMENUtunnelseg,SMENUStunnelseg,"ExeSeg16IcoL",".mps",4852,SMENUDtunnelseg));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("cleanseg","",Cmdcleanseg,SMENUcleanseg,SMENUScleanseg,"ExeSeg16IcoL",".mps",4802,SMENUDcleanseg));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("polseg","",Cmdpolseg,SMENUpolseg,SMENUSpolseg,"ExeSeg16IcoL",".mpa",4892,SMENUDpolseg));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("rasseg","",Cmdrasseg,SMENUrasseg,SMENUSrasseg,"ExeSeg16IcoL",".mpr",4902,SMENUDrasseg));
 
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("intersectseg","",Cmdsegmentmapintersect,SMENUsintersection,SMENUSsintersection,"ExeSeg16IcoL",".mpa.mps",0,SMENUDsintersection));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("unionseg","",Cmdsegmentmapunion,SMENUsunion,SMENUSsunion,"ExeSeg16IcoL",".mpa.mps",0,SMENUDsunion));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("diffseg","",Cmdsegmentmapdifference,SMENUsdifference,SMENUSsdifference,"ExeSeg16IcoL",".mpa.mps",0,SMENUDsdifference));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("voronoi","",Cmdsegmentmapvoronoi,SMENUvoronoi,SMENUSvoronoi,"ExeSeg16IcoL",".mpp",0,SMENUDvoronoi));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("symdiffseg","",Cmdsegmentmapsymetricdifference,SMENUssymetricdifference,SMENUSssymetricdifference,"ExeSeg16IcoL",".mpa.mps",0,SMENUDssymetricdifference));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("relateseg","",Cmdsegmentmaprelate,SMENUsegrelate,SMENUSsegrelate,"ExeSeg16IcoL",".mps",0,SMENUDsegrelate));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("tin","",Cmdsegmentmaptin,SMENUsegTIN,SMENUSsegTIN,"ExeSeg16IcoL",".mps",0,SMENUDsegTIN));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("isolines","",Cmdsegmentisoline,TR("&Segment Operations..&Iso Lines"),TR("Iso lines"),"ExeSeg16IcoL",".mps",0,TR("Creates Iso lines from a value raster map")));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("intersectseg","",Cmdsegmentmapintersect,SMENUsintersection,SMENUSsintersection,"ExeSeg16IcoL",".mpa.mps",0,SMENUDsintersection));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("unionseg","",Cmdsegmentmapunion,SMENUsunion,SMENUSsunion,"ExeSeg16IcoL",".mpa.mps",0,SMENUDsunion));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("diffseg","",Cmdsegmentmapdifference,SMENUsdifference,SMENUSsdifference,"ExeSeg16IcoL",".mpa.mps",0,SMENUDsdifference));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("voronoi","",Cmdsegmentmapvoronoi,SMENUvoronoi,SMENUSvoronoi,"ExeSeg16IcoL",".mpp",0,SMENUDvoronoi));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("symdiffseg","",Cmdsegmentmapsymetricdifference,SMENUssymetricdifference,SMENUSssymetricdifference,"ExeSeg16IcoL",".mpa.mps",0,SMENUDssymetricdifference));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("relateseg","",Cmdsegmentmaprelate,SMENUsegrelate,SMENUSsegrelate,"ExeSeg16IcoL",".mps",0,SMENUDsegrelate));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("tin","",Cmdsegmentmaptin,SMENUsegTIN,SMENUSsegTIN,"ExeSeg16IcoL",".mps",0,SMENUDsegTIN));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("isolines","",Cmdsegmentisoline,TR("&Segment Operations..&Iso Lines"),TR("Iso lines"),"ExeSeg16IcoL",".mps",0,TR("Creates Iso lines from a value raster map")));
 
 return infosui;
 }
 
-extern "C" _export ILWIS::Module *getModuleInfo() {
-	ILWIS::Module *module = new ILWIS::Module("Segmentmap applications UI", "SegmentApplicationsUI.dll",ILWIS::Module::mi37,"1.0");
-	module->addMethod(ILWIS::Module::ifGetAppInfoUI, (void *)getApplicationInfoUI);  
+extern "C" _export ILWIS::Module *getModuleInfo(const FileName& fnModule) {
+	ILWIS::Module *module = new ILWIS::Module("Segmentmap applications UI", fnModule,ILWIS::Module::mi37,"1.0");
+	module->addMethod(ILWIS::Module::ifgetCommandInfoUI, (void *)getCommandInfoUI);  
 
 	return module;
 }
