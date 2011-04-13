2412d8ff4117776976dc1ce35d47640b5cf3fa92
Martin Schouwenburg <m.l.schouwenburg@utwente.nl>
2011-04-13 07:48:34 +0000

    updated moduleinfo method
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@932 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/HydroFlowApplicationsUI/apps.cpp
+++ Sources/HydroFlowApplicationsUI/apps.cpp
@@ -50,37 +50,37 @@ Last change:  WK   17 Sep 98    2:14 pm
 #include "Client\ilwis.h"
 #include "Headers\Hs\Applications.hs"
 
-InfoUIVector* getApplicationInfoUI() {
+InfoUIVector* getCommandInfoUI(ILWIS::Module *module) {
 	InfoUIVector *infosui = new InfoUIVector();
 	String ilwDir = IlwWinApp()->Context()->sIlwDir();
 	String hyrdoDemBase = ilwDir + "Scripts\\Hydro-DEM\\";
 
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("run '" + hyrdoDemBase + "dem_visualization'.isl","",Cmdrun,SMENUdem_visualization,SMENUSdem_visualization,"ExeMap16IcoL",".isl",8070,SMENUDdem_visualization));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("fillsinks","",Cmdfillsinks,SMENUfillsinks,SMENUSfillsinks,"ExeMap16IcoL",".mpr",8022,SMENUDfillsinks));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("flowdirection","",Cmdflowdirection,SMENUflowdirection,SMENUSflowdirection,"ExeMap16IcoL",".mpr",8012,SMENUDflowdirection));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("flowaccumulation","",Cmdflowaccumulation,SMENUflowaccumulation,SMENUSflowaccumulation,"ExeMap16IcoL",".mpr",8017,SMENUDflowaccumulation));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("demoptimization","",Cmddemoptimization,SMENUdemoptimization,SMENUSdemoptimization,"ExeMap16IcoL",".mpr",8027,SMENUDdemoptimization));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("topologicaloptimization","",Cmdtopologicaloptimization,SMENUtopologicaloptimization,SMENUStopologicaloptimization,"ExeMap16IcoL",".mpr",8062,SMENUDtopologicaloptimization));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("variablethresholdcomputation","",Cmdvariablethresholdcomputation,SMENUvariablethresholdcomputation,SMENUSvariablethresholdcomputation,"ExeMap16IcoL",".mpr",8067,SMENUDvariablethresholdcomputation));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("drainagenetworkextraction","",Cmddrainagenetworkextraction,SMENUdrainagenetworkextraction,SMENUSdrainagenetworkextraction,"ExeMap16IcoL",".mpr",8032,SMENUDdrainagenetworkextraction));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("drainagenetworkordering","",Cmddrainagenetworkordering,SMENUdrainagenetworkordering,SMENUSdrainagenetworkordering,"ExeMap16IcoL",".mpr",8037,SMENUDdrainagenetworkordering));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("catchmentextraction","",Cmdcatchmentextraction,SMENUcatchmentextraction,SMENUScatchmentextraction,"ExeMap16IcoL",".mpr",8047,SMENUDcatchmentextraction));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("catchmentmerge","",Cmdcatchmentmerge,SMENUcatchmentmerge,SMENUScatchmentmerge,"ExeMap16IcoL",".mpr",8052,SMENUDcatchmentmerge));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("overlandflowlength","",Cmdoverlandflowlength,SMENUoverlandflowlength,SMENUSoverlandflowlength,"ExeMap16IcoL",".mpr",8042,SMENUDoverlandflowlength));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("flowlengthtooutlet","",Cmdflowlengthtooutlet,SMENUflowlengthtooutlet,SMENUSflowlengthtooutlet,"ExeMap16IcoL",".mpr",8042,SMENUDflowlengthtooutlet));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("flowpathlongitudinalprofile","",Cmdflowpathlongitudinalprofile,SMENUflowpathlongitudinalprofile,SMENUSflowpathlongitudinalprofile,"ExeMap16IcoL",".mpr",8042,SMENUDflowpathlongitudinalprofile));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("run '" + hyrdoDemBase + "Compound_Parameter_Extraction'.isl","",Cmdrun,SMENUCompound_Parameter_Extraction,SMENUSCompound_Parameter_Extraction,"ExeMap16IcoL",".isl",8071,SMENUSCompound_Parameter_Extraction));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("hortonplots","",Cmdhortonplots,SMENUhortonplots,SMENUShortonplots,"ExeTbl16IcoL",".tbl",8057,SMENUDhortonplots));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("run '" + hyrdoDemBase + "aggregate_statistics'.isl","",Cmdrun ,SMENUaggregate_statistics,SMENUSaggregate_statistics,"ExeTbl16IcoL",".isl",8072,SMENUDaggregate_statistics));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("run '" + hyrdoDemBase + "cumulative_hypsometric_curve'.isl","",Cmdrun,SMENUcumulative_hypsometric_curve,SMENUScumulative_hypsometric_curve,"ExeTbl16IcoL",".isl",8073,SMENUDcumulative_hypsometric_curve));
-(*infosui).push_back(CommandHandlerUI::createApplicationInfo("run '" + hyrdoDemBase + "class_coverage_statistics'.isl","",Cmdrun,SMENUclass_coverage_statistics,SMENUSclass_coverage_statistics,"ExeTbl16IcoL",".isl",8074,SMENUDclass_coverage_statistics));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("run '" + hyrdoDemBase + "dem_visualization'.isl","",Cmdrun,SMENUdem_visualization,SMENUSdem_visualization,"ExeMap16IcoL",".isl",8070,SMENUDdem_visualization));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("fillsinks","",Cmdfillsinks,SMENUfillsinks,SMENUSfillsinks,"ExeMap16IcoL",".mpr",8022,SMENUDfillsinks));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("flowdirection","",Cmdflowdirection,SMENUflowdirection,SMENUSflowdirection,"ExeMap16IcoL",".mpr",8012,SMENUDflowdirection));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("flowaccumulation","",Cmdflowaccumulation,SMENUflowaccumulation,SMENUSflowaccumulation,"ExeMap16IcoL",".mpr",8017,SMENUDflowaccumulation));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("demoptimization","",Cmddemoptimization,SMENUdemoptimization,SMENUSdemoptimization,"ExeMap16IcoL",".mpr",8027,SMENUDdemoptimization));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("topologicaloptimization","",Cmdtopologicaloptimization,SMENUtopologicaloptimization,SMENUStopologicaloptimization,"ExeMap16IcoL",".mpr",8062,SMENUDtopologicaloptimization));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("variablethresholdcomputation","",Cmdvariablethresholdcomputation,SMENUvariablethresholdcomputation,SMENUSvariablethresholdcomputation,"ExeMap16IcoL",".mpr",8067,SMENUDvariablethresholdcomputation));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("drainagenetworkextraction","",Cmddrainagenetworkextraction,SMENUdrainagenetworkextraction,SMENUSdrainagenetworkextraction,"ExeMap16IcoL",".mpr",8032,SMENUDdrainagenetworkextraction));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("drainagenetworkordering","",Cmddrainagenetworkordering,SMENUdrainagenetworkordering,SMENUSdrainagenetworkordering,"ExeMap16IcoL",".mpr",8037,SMENUDdrainagenetworkordering));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("catchmentextraction","",Cmdcatchmentextraction,SMENUcatchmentextraction,SMENUScatchmentextraction,"ExeMap16IcoL",".mpr",8047,SMENUDcatchmentextraction));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("catchmentmerge","",Cmdcatchmentmerge,SMENUcatchmentmerge,SMENUScatchmentmerge,"ExeMap16IcoL",".mpr",8052,SMENUDcatchmentmerge));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("overlandflowlength","",Cmdoverlandflowlength,SMENUoverlandflowlength,SMENUSoverlandflowlength,"ExeMap16IcoL",".mpr",8042,SMENUDoverlandflowlength));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("flowlengthtooutlet","",Cmdflowlengthtooutlet,SMENUflowlengthtooutlet,SMENUSflowlengthtooutlet,"ExeMap16IcoL",".mpr",8042,SMENUDflowlengthtooutlet));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("flowpathlongitudinalprofile","",Cmdflowpathlongitudinalprofile,SMENUflowpathlongitudinalprofile,SMENUSflowpathlongitudinalprofile,"ExeMap16IcoL",".mpr",8042,SMENUDflowpathlongitudinalprofile));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("run '" + hyrdoDemBase + "Compound_Parameter_Extraction'.isl","",Cmdrun,SMENUCompound_Parameter_Extraction,SMENUSCompound_Parameter_Extraction,"ExeMap16IcoL",".isl",8071,SMENUSCompound_Parameter_Extraction));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("hortonplots","",Cmdhortonplots,SMENUhortonplots,SMENUShortonplots,"ExeTbl16IcoL",".tbl",8057,SMENUDhortonplots));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("run '" + hyrdoDemBase + "aggregate_statistics'.isl","",Cmdrun ,SMENUaggregate_statistics,SMENUSaggregate_statistics,"ExeTbl16IcoL",".isl",8072,SMENUDaggregate_statistics));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("run '" + hyrdoDemBase + "cumulative_hypsometric_curve'.isl","",Cmdrun,SMENUcumulative_hypsometric_curve,SMENUScumulative_hypsometric_curve,"ExeTbl16IcoL",".isl",8073,SMENUDcumulative_hypsometric_curve));
+(*infosui).push_back(CommandHandlerUI::createCommandInfo("run '" + hyrdoDemBase + "class_coverage_statistics'.isl","",Cmdrun,SMENUclass_coverage_statistics,SMENUSclass_coverage_statistics,"ExeTbl16IcoL",".isl",8074,SMENUDclass_coverage_statistics));
 
 	return infosui;
 }
 
-extern "C" _export ILWIS::Module *getModuleInfo() {
-	ILWIS::Module *module = new ILWIS::Module("Hydrological flow applications UI", "HydroFlowUI.dll",ILWIS::Module::mi37,"1.0");
-	module->addMethod(ILWIS::Module::ifGetAppInfoUI, (void *)getApplicationInfoUI);  
+extern "C" _export ILWIS::Module *getModuleInfo(const FileName& fnModule) {
+	ILWIS::Module *module = new ILWIS::Module("Hydrological flow applications UI", fnModule,ILWIS::Module::mi37,"1.0");
+	module->addMethod(ILWIS::Module::ifgetCommandInfoUI, (void *)getCommandInfoUI);  
 
 	return module;
 }
