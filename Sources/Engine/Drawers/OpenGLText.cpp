ec50300c59f977397328b805adb2ea3c10a92672
Bas Retsios <v.retsios@utwente.nl>
2011-07-07 09:11:45 +0000

    Added option for color, corrected the option to have a zoom-level dependent or independent font.
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@1027 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/Engine/Drawers/OpenGLText.cpp
+++ Sources/Engine/Drawers/OpenGLText.cpp
@@ -6,7 +6,13 @@
 
 #define LIST_SIZE 96
 
-OpenGLText::OpenGLText(ILWIS::RootDrawer *rootdrawer,const String& name, int h, bool fixed) : height(h), actualHeight(iUNDEF), fixedSize(fixed),font(0) {
+OpenGLText::OpenGLText(ILWIS::RootDrawer *rootdrawer,const String& name, int height, bool fixed, double horShift, double verShift)
+: fontHeight(height)
+, fixedSize(fixed)
+, horizontalShift(horShift)
+, verticalShift(verShift)
+, font(0)
+{
 	FileName fn(name);
 	if ( !fn.fExist()){
 		TCHAR szPath[MAX_PATH];
@@ -16,13 +22,19 @@ OpenGLText::OpenGLText(ILWIS::RootDrawer *rootdrawer,const String& name, int h, 
     }
 	//font = new FTTextureFont(fn.sFullPath().scVal());
 	font = new FTPolygonFont(fn.sFullPath().scVal());
+	font->FaceSize(fontHeight);
 	color = Color(0,0,0);
-	scale = 0.18;
-	double  ratio = ((double)height / 100.0)* rootdrawer->getCoordBoundsZoom().height();
-	int h1 = ratio;
-	if ( (h1 != actualHeight && !fixedSize) || actualHeight == iUNDEF) {
-		actualHeight = h1;
-		font->FaceSize(actualHeight);
+	if (fixedSize) {
+		if (rootdrawer->getCoordBoundsZoom().height() > rootdrawer->getCoordBoundsZoom().width())
+			scale = rootdrawer->getCoordBoundsZoom().height() / (double)rootdrawer->getViewPort().Row; // result is the meters/pixel or the degrees/pixel of the current zoom level
+		else
+			scale = rootdrawer->getCoordBoundsZoom().width() / (double)rootdrawer->getViewPort().Col; // result is the meters/pixel or the degrees/pixel of the current zoom level
+	}
+	else {
+		if (rootdrawer->getMapCoordBounds().height() > rootdrawer->getMapCoordBounds().width())
+			scale = rootdrawer->getMapCoordBounds().height() / (double)rootdrawer->getViewPort().Row; // result is the meters/pixel or the degrees/pixel of the image
+		else
+			scale = rootdrawer->getMapCoordBounds().width() / (double)rootdrawer->getViewPort().Col; // result is the meters/pixel or the degrees/pixel of the image
 	}
 }
 
@@ -32,19 +44,23 @@ OpenGLText::~OpenGLText() {
 
 void OpenGLText::renderText(const Coordinate& c, const String& text) {
 	glPushMatrix();
-	glScaled(scale, scale, 1);
+	glScaled(scale, scale, 1); // with this the GL space is temporarily expressed in pixels
 	glColor3d(color.redP(), color.greenP(), color.blueP());
 	glEnable (GL_POLYGON_SMOOTH);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 	glEnable(GL_BLEND);
 	glHint (GL_POLYGON_SMOOTH, GL_DONT_CARE);
-	font->Render(text.scVal(),text.size(),FTPoint(c.x / scale,c.y / scale,c.z));
+	font->Render(text.scVal(),text.size(),FTPoint(horizontalShift + c.x / scale, verticalShift + c.y / scale, c.z));
 	glPopMatrix();
 }
 
+void OpenGLText::setColor(const Color & clr) {
+	color = clr;
+}
+
 double OpenGLText::getHeight() const{
-	if ( actualHeight != iUNDEF)
-		return actualHeight * scale;
+	if ( fontHeight != iUNDEF)
+		return fontHeight * scale;
 	return 0;
 }
 
