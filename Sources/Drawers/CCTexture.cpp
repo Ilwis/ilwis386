a159d44577a8d26078b4c51a83a018239b655f2f
Bas Retsios <v.retsios@utwente.nl>
2011-10-21 10:47:50 +0000

    1. Enable color composite for any type of map (not just Byte/Image) 2. Apply the stretch values (if available) also when the window is first opened (not only after the stretch form is used)
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@1158 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/Drawers/CCTexture.cpp
+++ Sources/Drawers/CCTexture.cpp
@@ -36,7 +36,7 @@ CCTexture::~CCTexture()
 
 void CCTexture::CreateTexture(DrawerContext * drawerContext, bool fInThread, volatile bool * fDrawStop)
 {
-	fValue = 0 != mp->dm()->pdvi() || 0 != mp->dm()->pdvr();
+	fValue = 0 != mpl[0]->dm()->pdvi() || 0 != mpl[0]->dm()->pdvr();
 	fAttTable = false;
 	texture_data = new char [(sizeX / zoomFactor) * (sizeY / zoomFactor) * 4];
 	this->valid = DrawTexture(offsetX, offsetY, sizeX, sizeY, zoomFactor, texture_data, fDrawStop);
@@ -60,7 +60,41 @@ void CCTexture::CreateTexture(DrawerContext * drawerContext, bool fInThread, vol
 	boolean oldVal;
 	glGetBooleanv(GL_MAP_COLOR, &oldVal);
 	glPixelTransferf(GL_MAP_COLOR, 0);
+	RangeReal rrFromR = data->ccMaps[0].rr;
+	RangeReal rrToR = mpl[data->ccMaps[0].index]->rrMinMax();
+	if (rrFromR.fValid() && rrToR.fValid()) {
+		float fFactR = (rrToR.rWidth()) / (rrFromR.rWidth());
+		float fOffR = (rrToR.rLo() - rrFromR.rLo()) / rrFromR.rWidth();
+		glPixelTransferf(GL_RED_SCALE, fFactR);
+		glPixelTransferf(GL_RED_BIAS, fOffR);
+	}
+
+	RangeReal rrFromG = data->ccMaps[1].rr;
+	RangeReal rrToG = mpl[data->ccMaps[1].index]->rrMinMax();
+	if (rrFromG.fValid() && rrToG.fValid()) {
+		float fFactG = (rrToG.rWidth()) / (rrFromG.rWidth());
+		float fOffG = (rrToG.rLo() - rrFromG.rLo()) / rrFromG.rWidth();
+		glPixelTransferf(GL_GREEN_SCALE, fFactG);
+		glPixelTransferf(GL_GREEN_BIAS, fOffG);
+	}
+
+	RangeReal rrFromB = data->ccMaps[2].rr;
+	RangeReal rrToB = mpl[data->ccMaps[2].index]->rrMinMax();
+	if (rrFromB.fValid() && rrToB.fValid()) {
+		float fFactB = (rrToB.rWidth()) / (rrFromB.rWidth());
+		float fOffB = (rrToB.rLo() - rrFromB.rLo()) / rrFromB.rWidth();
+		glPixelTransferf(GL_BLUE_SCALE, fFactB);
+		glPixelTransferf(GL_BLUE_BIAS, fOffB);
+	}
+
 	glTexImage2D( GL_TEXTURE_2D, 0, 4, sizeX / zoomFactor, sizeY / zoomFactor, 0, GL_RGBA, GL_UNSIGNED_BYTE, texture_data);
+
+	glPixelTransferf(GL_RED_SCALE, 1);
+	glPixelTransferf(GL_RED_BIAS, 0);
+	glPixelTransferf(GL_GREEN_SCALE, 1);
+	glPixelTransferf(GL_GREEN_BIAS, 0);
+	glPixelTransferf(GL_BLUE_SCALE, 1);
+	glPixelTransferf(GL_BLUE_BIAS, 0);
 	glPixelTransferf(GL_MAP_COLOR, oldVal);
 	fRepresentationChanged = false;
 	if (fInThread)
@@ -69,7 +103,7 @@ void CCTexture::CreateTexture(DrawerContext * drawerContext, bool fInThread, vol
 
 void CCTexture::ReCreateTexture(DrawerContext * drawerContext, bool fInThread, volatile bool * fDrawStop)
 {
-	fValue = 0 != mp->dm()->pdvi() || 0 != mp->dm()->pdvr();
+	fValue = 0 != mpl[0]->dm()->pdvi() || 0 != mpl[0]->dm()->pdvr();
 	fAttTable = false;
 	this->dirty = !DrawTexture(offsetX, offsetY, sizeX, sizeY, zoomFactor, texture_data, fDrawStop);
 
@@ -137,48 +171,106 @@ bool CCTexture::DrawTexture(long offsetX, long offsetY, long texSizeX, long texS
 	if (sizeX == 0 || sizeY == 0)
 		return false;
 
+	ValueRange vr = mpl[0]->vr();
+	if (mpl[0]->dm()->pdbool())
+		vr = ValueRange();
+	bool fRealMap;
+	if (vr.fValid()) // when integers are not good enough to represent the map treat it as a real map
+		fRealMap = (vr->rStep() < 1) || (vr->stUsed() == stREAL);
+	else
+		fRealMap = false;
+
 	if (*fDrawStop)
 		return false;
 
+	double rFactR = 1;
+	double rOffR = 0;
+	double rFactG = 1;
+	double rOffG = 0;
+	double rFactB = 1;
+	double rOffB = 0;
 	RangeReal rrFromR = mpl[data->ccMaps[0].index]->rrMinMax();
 	RangeReal rrFromG = mpl[data->ccMaps[1].index]->rrMinMax();
 	RangeReal rrFromB = mpl[data->ccMaps[2].index]->rrMinMax();
 	RangeReal rrTo = RangeReal(0, 255);
-
-	byte stretchedValues[768];
-	for(int i=0; i < 256; ++i) {
-		stretchedValues[i] = stretch(i, rrFromR, rrTo);
-		stretchedValues[i + 256] = stretch(i, rrFromG, rrTo);
-		stretchedValues[i + 512] = stretch(i, rrFromB, rrTo);
+	if (rrFromR.fValid()) {
+		rFactR = (rrTo.rWidth()) / (rrFromR.rWidth());
+		rOffR = rrFromR.rLo();
+	}
+	if (rrFromG.fValid()) {
+		rFactG = (rrTo.rWidth()) / (rrFromG.rWidth());
+		rOffG = rrFromG.rLo();
+	}
+	if (rrFromB.fValid()) {
+		rFactB = (rrTo.rWidth()) / (rrFromB.rWidth());
+		rOffB = rrFromB.rLo();
 	}
 
 	if (zoomFactor == 1) // 1:1 a pixel is a rastel; expected in outbuf: sizeX * sizeY * 4 bytes (for RGBA colors)
 	{
-		ByteBuf bufIn1(sizeX),bufIn2(sizeX),bufIn3(sizeX);
-		byte * ptrBufIn1 = bufIn1.buf();
-		byte * ptrBufIn2 = bufIn2.buf();
-		byte * ptrBufIn3 = bufIn3.buf();
-		LongBuf bufColor(sizeX);
-		long * ptrBufColor = bufColor.buf();
-		for (long iDataInYPos = 0; iDataInYPos < sizeY; ++iDataInYPos) 
+		if (fRealMap) 
 		{
-			if (*fDrawStop) {
-				return false;
+			RealBuf bufIn1(sizeX);
+			RealBuf bufIn2(sizeX);
+			RealBuf bufIn3(sizeX);
+			double * ptrBufIn1 = bufIn1.buf();
+			double * ptrBufIn2 = bufIn2.buf();
+			double * ptrBufIn3 = bufIn3.buf();
+			LongBuf bufColor(sizeX);
+			long * ptrBufColor = bufColor.buf();
+			memset(bufIn1.buf(), 0, sizeX * 8); // to prevent NAN values in bufIn.
+			memset(bufIn2.buf(), 0, sizeX * 8);
+			memset(bufIn3.buf(), 0, sizeX * 8);
+			for (long iDataInYPos = 0; iDataInYPos < sizeY; ++iDataInYPos)
+			{
+				if (*fDrawStop) {
+					return false;
+				}
+				mpl[data->ccMaps[0].index]->GetLineVal(iDataInYPos + offsetY, bufIn1, offsetX, sizeX);
+				mpl[data->ccMaps[1].index]->GetLineVal(iDataInYPos + offsetY, bufIn2, offsetX, sizeX);
+				mpl[data->ccMaps[2].index]->GetLineVal(iDataInYPos + offsetY, bufIn3, offsetX, sizeX);
+				for (int i = 0; i < sizeX; ++i) {
+					byte v1 = byteConv((ptrBufIn1[i] - rOffR) * rFactR);
+					byte v2 = byteConv((ptrBufIn2[i] - rOffG) * rFactG);
+					byte v3 = byteConv((ptrBufIn3[i] - rOffB) * rFactB);
+					ptrBufColor[i] = (v1) | (v2 << 8) | (v3 << 16);
+				}
+				PutLine(bufIn1, bufColor, iDataInYPos, texSizeX, outbuf);
 			}
-
-			mpl[data->ccMaps[0].index]->GetLineRaw(iDataInYPos + offsetY, bufIn1, offsetX, sizeX);
-			mpl[data->ccMaps[1].index]->GetLineRaw(iDataInYPos + offsetY, bufIn2, offsetX, sizeX);
-			mpl[data->ccMaps[2].index]->GetLineRaw(iDataInYPos + offsetY, bufIn3, offsetX, sizeX);
-
-			for(int i = 0; i < sizeX; ++i) {
-				byte v1 = stretchedValues[ptrBufIn1[i]];
-				byte v2 = stretchedValues[ptrBufIn2[i] + 256];
-				byte v3 = stretchedValues[ptrBufIn3[i] + 512];
-				ptrBufColor[i] = (v1) | (v2 << 8) | (v3 << 16);
-			}
-
-			PutLine(bufColor, bufColor, iDataInYPos, texSizeX, outbuf);
 		}
+		else 
+		{ // !fRealMap
+			LongBuf bufIn1(sizeX);
+			LongBuf bufIn2(sizeX);
+			LongBuf bufIn3(sizeX);
+			long * ptrBufIn1 = bufIn1.buf();
+			long * ptrBufIn2 = bufIn2.buf();
+			long * ptrBufIn3 = bufIn3.buf();
+			LongBuf bufColor(sizeX);
+			long * ptrBufColor = bufColor.buf();
+			for (long iDataInYPos = 0; iDataInYPos < sizeY; ++iDataInYPos) 
+			{
+				if (*fDrawStop) {
+					return false;
+				}
+				if (fValue && !fAttTable) {
+					mpl[data->ccMaps[0].index]->GetLineVal(iDataInYPos + offsetY, bufIn1, offsetX, sizeX);
+					mpl[data->ccMaps[1].index]->GetLineVal(iDataInYPos + offsetY, bufIn2, offsetX, sizeX);
+					mpl[data->ccMaps[2].index]->GetLineVal(iDataInYPos + offsetY, bufIn3, offsetX, sizeX);
+				} else {
+					mpl[data->ccMaps[0].index]->GetLineRaw(iDataInYPos + offsetY, bufIn1, offsetX, sizeX);
+					mpl[data->ccMaps[1].index]->GetLineRaw(iDataInYPos + offsetY, bufIn2, offsetX, sizeX);
+					mpl[data->ccMaps[2].index]->GetLineRaw(iDataInYPos + offsetY, bufIn3, offsetX, sizeX);
+				}
+				for (int i = 0; i < sizeX; ++i) {
+					byte v1 = byteConv((ptrBufIn1[i] - rOffR) * rFactR);
+					byte v2 = byteConv((ptrBufIn2[i] - rOffG) * rFactG);
+					byte v3 = byteConv((ptrBufIn3[i] - rOffB) * rFactB);
+					ptrBufColor[i] = (v1) | (v2 << 8) | (v3 << 16);
+				}
+				PutLine(bufIn1, bufColor, iDataInYPos, texSizeX, outbuf);
+			}
+		}                                                                         
 	}
 	else // zoomFactor > 1; expected in outbuf: sizeX * sizeY * 4 / (zoomFactor * zoomFactor) bytes (for RGB colors)
 	{
@@ -204,42 +296,73 @@ bool CCTexture::DrawTexture(long offsetX, long offsetY, long texSizeX, long texS
 		// Note that offsetX, offsetY, texSizeX, texSizeY are ^2, so they're alvays perfectly divisible by pow(2)
 		// For division of sizeX and sizeY we take the ceiling, otherwise the last line / column is missing in a non-perfect division.
 
-		ByteBuf bufIn1(sizeX),bufIn2(sizeX),bufIn3(sizeX);
-		byte * ptrBufIn1 = bufIn1.buf();
-		byte * ptrBufIn2 = bufIn2.buf();
-		byte * ptrBufIn3 = bufIn3.buf();
-		LongBuf bufColor(xSizeOut);
-		long * ptrBufColor = bufColor.buf();
-
-		for (long iDataOutYPos = 0, iDataInYPos = 0; iDataOutYPos < ySizeOut; ++iDataOutYPos, iDataInYPos += zoomFactor)
+		if (fRealMap) 
 		{
-			if (*fDrawStop) {
-				return false;
-			}
-			mpl[data->ccMaps[0].index]->GetLineRaw(iDataInYPos + offsetY, bufIn1, offsetX, sizeX,iPyrLayer);
-			mpl[data->ccMaps[1].index]->GetLineRaw(iDataInYPos + offsetY, bufIn2, offsetX, sizeX,iPyrLayer);
-			mpl[data->ccMaps[2].index]->GetLineRaw(iDataInYPos + offsetY, bufIn3, offsetX, sizeX,iPyrLayer);
+			RealBuf bufIn1(sizeX);
+			RealBuf bufIn2(sizeX);
+			RealBuf bufIn3(sizeX);
+			double * ptrBufIn1 = bufIn1.buf();
+			double * ptrBufIn2 = bufIn2.buf();
+			double * ptrBufIn3 = bufIn3.buf();
+			LongBuf bufColor(xSizeOut);
+			long * ptrBufColor = bufColor.buf();
+			memset(ptrBufIn1, 0, sizeX * 8); // to prevent NAN values in bufIn.
+			memset(ptrBufIn2, 0, sizeX * 8);
+			memset(ptrBufIn3, 0, sizeX * 8);
 
-			for (long iDataOutXPos = 0, iDataInXPos = 0; iDataOutXPos < xSizeOut; ++iDataOutXPos, iDataInXPos += zoomFactor) {
-				byte v1 = stretchedValues[ptrBufIn1[iDataInXPos]];
-				byte v2 = stretchedValues[ptrBufIn2[iDataInXPos] + 256];
-				byte v3 = stretchedValues[ptrBufIn3[iDataInXPos] + 512];
-				ptrBufColor[iDataOutXPos] = (v1) | (v2 << 8) | (v3 << 16);
+			for (long iDataOutYPos = 0, iDataInYPos = 0; iDataOutYPos < ySizeOut; ++iDataOutYPos, iDataInYPos += zoomFactor)
+			{
+				if (*fDrawStop) {
+					return false;
+				}
+				mpl[data->ccMaps[0].index]->GetLineVal(iDataInYPos + offsetY, bufIn1, offsetX, sizeX, iPyrLayer);
+				mpl[data->ccMaps[1].index]->GetLineVal(iDataInYPos + offsetY, bufIn2, offsetX, sizeX, iPyrLayer);
+				mpl[data->ccMaps[2].index]->GetLineVal(iDataInYPos + offsetY, bufIn3, offsetX, sizeX, iPyrLayer);
+				for (long iDataOutXPos = 0, iDataInXPos = 0; iDataOutXPos < xSizeOut; ++iDataOutXPos, iDataInXPos += zoomFactor) {
+					byte v1 = byteConv((ptrBufIn1[iDataInXPos] - rOffR) * rFactR);
+					byte v2 = byteConv((ptrBufIn2[iDataInXPos] - rOffG) * rFactG);
+					byte v3 = byteConv((ptrBufIn3[iDataInXPos] - rOffB) * rFactB);
+					ptrBufColor[iDataOutXPos] = (v1) | (v2 << 8) | (v3 << 16);
+				}
+				PutLine(bufColor, bufColor, iDataOutYPos, texSizeX, outbuf);
 			}
-
-			PutLine(bufColor, bufColor, iDataOutYPos, texSizeX, outbuf);
 		}
+		else // !fRealMap
+		{
+			LongBuf bufIn1(sizeX);
+			LongBuf bufIn2(sizeX);
+			LongBuf bufIn3(sizeX);
+			long * ptrBufIn1 = bufIn1.buf();
+			long * ptrBufIn2 = bufIn2.buf();
+			long * ptrBufIn3 = bufIn3.buf();
+			LongBuf bufColor(xSizeOut);
+			long * ptrBufColor = bufColor.buf();
+			for (long iDataOutYPos = 0, iDataInYPos = 0; iDataOutYPos < ySizeOut; ++iDataOutYPos, iDataInYPos += zoomFactor)
+			{
+				if (*fDrawStop) {
+					return false;
+				}
+				if (fValue && !fAttTable) {
+					mpl[data->ccMaps[0].index]->GetLineVal(iDataInYPos + offsetY, bufIn1, offsetX, sizeX, iPyrLayer);
+					mpl[data->ccMaps[1].index]->GetLineVal(iDataInYPos + offsetY, bufIn2, offsetX, sizeX, iPyrLayer);
+					mpl[data->ccMaps[2].index]->GetLineVal(iDataInYPos + offsetY, bufIn3, offsetX, sizeX, iPyrLayer);
+				} else {
+					mpl[data->ccMaps[0].index]->GetLineRaw(iDataInYPos + offsetY, bufIn1, offsetX, sizeX, iPyrLayer);
+					mpl[data->ccMaps[1].index]->GetLineRaw(iDataInYPos + offsetY, bufIn2, offsetX, sizeX, iPyrLayer);
+					mpl[data->ccMaps[2].index]->GetLineRaw(iDataInYPos + offsetY, bufIn3, offsetX, sizeX, iPyrLayer);
+				}
+				for (long iDataOutXPos = 0, iDataInXPos = 0; iDataOutXPos < xSizeOut; ++iDataOutXPos, iDataInXPos += zoomFactor) {
+					byte v1 = byteConv((ptrBufIn1[iDataInXPos] - rOffR) * rFactR);
+					byte v2 = byteConv((ptrBufIn2[iDataInXPos] - rOffG) * rFactG);
+					byte v3 = byteConv((ptrBufIn3[iDataInXPos] - rOffB) * rFactB);
+					ptrBufColor[iDataOutXPos] = (v1) | (v2 << 8) | (v3 << 16);
+				}
+
+				PutLine(bufColor, bufColor, iDataOutYPos, texSizeX, outbuf);
+			}
+		}                 
 	}
 	return true;
-}
-
-double CCTexture::stretch(double v, const RangeReal& rrFrom, const RangeReal& rrTo) {
-	if ( rrFrom.fValid() && rrTo.fValid()) {
-		double rFact = (rrTo.rWidth()) / (rrFrom.rWidth());
-		double rOff = rrTo.rLo();
-		return byteConv(rFact * (v - rrFrom.rLo()) + rOff);
-	}
-	return v;
 }
 
 void CCTexture::BindMe(DrawerContext * drawerContext)
--- Sources/Drawers/CCTexture.h
+++ Sources/Drawers/CCTexture.h
@@ -26,7 +26,6 @@ namespace ILWIS {
 		//virtual bool fValid();
 
 	private:
-		void linearStretch(ByteBuf& buf1, ByteBuf& buf2, ByteBuf& buf3, RangeReal ranges[] );
 		double stretch(double v, const RangeReal& rrFrom, const RangeReal& rrTo);
 		void BindMe(DrawerContext * drawerContext);
 		bool DrawTexture(long offsetX, long offsetY, long texSizeX, long texSizeY, unsigned int zoomFactor, char * outbuf, volatile bool* fDrawStop);
