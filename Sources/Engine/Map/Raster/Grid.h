c0cdc49b9352bf44d1bce19be4c9ce550cfcae5c
Martin Schouwenburg <m.l.schouwenburg@utwente.nl>
2012-01-26 09:04:40 +0000

    several bugs in this class are solved, works now as intended
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@1383 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/Engine/Map/Raster/Grid.h
+++ Sources/Engine/Map/Raster/Grid.h
@@ -5,28 +5,30 @@
 namespace ILWIS {
 template <class T> class Grid {
 public:
-	Grid(int ySize, int xSize, unsigned long maxBSize=1e8) : maxBlockSize(maxBSize) : valid(false){
+	Grid(int ySize, int xSize, unsigned long maxBSize=1e7) : maxBlockSize(maxBSize),valid(false){
 		elementSize = sizeof(T);
 		setSize(ySize,xSize);
 	}
 
-	Grid(const Map& mp) : valid(false) {
+	Grid(const Map& mp, unsigned long maxBSize=1e7) : valid(false), maxBlockSize(maxBSize) {
+		elementSize = sizeof(T);
 		RowCol rc = mp->rcSize();
 		setSize(rc.Row, rc.Col);
 		FileName fnData(mp->fnObj,".mp#");
-		ifstream infile(fnData.sPhysicalPath());
-		int noOfBunsigned long sz = rc.Row * rc.Col * elementSize;
-		int count;
-		while(true && infFile.is_open() && inFile.isGood()) {
-			if ( sz > blockSize)
+		ifstream infile(fnData.sPhysicalPath().c_str());
+		unsigned long blockSize = linesPerBlock * rc.Col * elementSize;
+		int count = 0;
+		unsigned long szLeft = sz;
+		while(true && infile.is_open() && infile.good()) {
+			if ( szLeft > blockSize)
 				infile.read((char *)values[count],blockSize);
 			else {
-				infile.read((char *)values[count],sz);
+				infile.read((char *)values[count],szLeft);
 				valid = true;
 				break;
 			}
 			++count;
-			sz -= blockSize;
+			szLeft -= blockSize;
 		}
 	}
 
@@ -35,22 +37,24 @@ public:
 	}
 
 	void setSize(int y, int x) {
-		unsigned long sz = xSize * ySize * elementSize;
-		int linesPerBlock = maxBlockSize / ( xSize * elementSize);
-		unsigned long blockSize = linesPerBlock * xSize * elementSize;
+		sz = y * x * elementSize;
+		lineSize = x;
+		unsigned long szLeft = sz;
+		linesPerBlock = maxBlockSize / ( x * elementSize);
+		unsigned long blockSize = linesPerBlock * x * elementSize;
 		noOfBlocks = sz / blockSize + 1;
 		values = new T*[noOfBlocks];
 		int count = 0;
 		while(true) {
-			if ( sz > blockSize)
+			if ( szLeft > blockSize)
 				values[count] =  new T[blockSize];
 			else {
-				values[count]  = new T[sz];
+				values[count]  = new T[szLeft];
 				valid = true;
 				break;
 			}
 			++count;
-			sz -= blockSize;
+			szLeft -= blockSize;
 		}
 	}
 
@@ -64,21 +68,25 @@ public:
 		delete [] values;
 	}
 
-	T& at(int y, int x){
-		values[ y / linesPerBlock] + elementSize * x;		
+	T& at(int y, int x) const{
+		int block = y / linesPerBlock;
+		int rest = y % linesPerBlock;
+		return values[block][ rest * lineSize +  x];		
 	}
 
 	T* at(int y) {
-		values[ y / linesPerBlock];
+		return values[ y / linesPerBlock];
 	}
 
 private:
 	const long maxBlockSize;
+	long lineSize;
 	T **values;
 	int linesPerBlock;
 	size_t elementSize;
 	int noOfBlocks;
 	bool valid;
+	unsigned long sz;
 
 
 };
