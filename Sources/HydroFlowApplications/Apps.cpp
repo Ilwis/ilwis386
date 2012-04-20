0d02ca290dddbdb4107438149e95bd58da72c67c
Martin Schouwenburg <m.l.schouwenburg@utwente.nl>
2012-04-20 06:58:23 +0000

    version number
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@1527 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/HydroFlowApplications/Apps.cpp
+++ Sources/HydroFlowApplications/Apps.cpp
@@ -42,7 +42,7 @@ InfoVector* getCommandInfo() {
 }
 
 extern "C" _export ILWIS::Module *getModuleInfo(const FileName& fnModule) {
-	ILWIS::Module *module = new ILWIS::Module("Hydrological flow Applications", fnModule,ILWIS::Module::mi37,"1.0");
+	ILWIS::Module *module = new ILWIS::Module("Hydrological flow Applications", fnModule,ILWIS::Module::mi37,"1.1");
 	module->addMethod(ILWIS::Module::ifgetCommandInfo, (void *)getCommandInfo);  
 
 	return module;
