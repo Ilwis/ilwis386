9a8ceb14a91a6d57ff7e869c7b2f8469f6cd5610
Bas Retsios <v.retsios@utwente.nl>
2012-05-03 22:25:20 +0000

    Chromosome is assigned two random weights that sum to 1.
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@1554 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/MOGA/GAChromosome.cpp
+++ Sources/MOGA/GAChromosome.cpp
@@ -2,14 +2,19 @@
 #include "LandAllocation.h"
 //#include "Engine\Base\DataObjects\strng.h"
 #include "Headers\toolspch.h"
+#include "Engine\Base\Algorithm\Random.h"
 
 GAChromosome::GAChromosome()
 : m_dFitness(0)
+, m_w1(random())
+, m_w2(1.0 - m_w1)
 {
 }
 
 GAChromosome::GAChromosome(LandAllocation * context, FitnessFunc fitnessFunc, ScoreFunc scoreFunc, bool fInitialize)
 : m_dFitness(0)
+, m_w1(random())
+, m_w2(1.0 - m_w1)
 {
 	if (fInitialize)
 		context->Initializer(*this); //will set the genes of each chromosome
@@ -20,12 +25,22 @@ GAChromosome::~GAChromosome()
 {
 }
 
-void GAChromosome::CopyChromosome(GAChromosome newChromosome)
+void GAChromosome::operator = (const GAChromosome& chromosome)
 {
 	clear();
-	int iLength = newChromosome.size();
+	int iLength = chromosome.size();
 	for(int i = 0; i < iLength; ++i)
-		push_back(newChromosome[i]);
+		push_back(chromosome[i]);
+	m_dFitness = chromosome.m_dFitness;
+}
+
+void GAChromosome::CopyChromosome(GAChromosome & chromosome)
+{
+	clear();
+	int iLength = chromosome.size();
+	for(int i = 0; i < iLength; ++i)
+		push_back(chromosome[i]);
+	m_dFitness = chromosome.m_dFitness;
 }
     
 int GAChromosome::HasThisGene(unsigned int Gene)
--- Sources/MOGA/GAChromosome.h
+++ Sources/MOGA/GAChromosome.h
@@ -7,7 +7,7 @@
 class GAChromosome;
 class LandAllocation;
 
-typedef double (IlwisObjectVirtual::*ScoreFunc)(int demandIndex, int facilityIndex);
+typedef double (IlwisObjectVirtual::*ScoreFunc)(int demandIndex, int facilityIndex, double w1, double w2);
 typedef void (IlwisObjectVirtual::*FitnessFunc)(GAChromosome & chromosome, LandAllocation * context, ScoreFunc scoreFunc);
 
 class GAChromosome : public std::vector<unsigned int>
@@ -16,13 +16,18 @@ public:
 	GAChromosome();
 	GAChromosome(LandAllocation * context, FitnessFunc fitnessFunc, ScoreFunc scoreFunc, bool fInitialize);
 	virtual ~GAChromosome();
-	void CopyChromosome(GAChromosome newChromosome); 
+	void CopyChromosome(GAChromosome & chromosome); 
 	int HasThisGene(unsigned int Gene);
 	double rGetFitness();
+	const double w1() {return m_w1;};
+	const double w2() {return m_w2;};
 	void SetFitness(double value);
 	bool operator < (const GAChromosome& chromosome) const;
+	void operator = (const GAChromosome& chromosome);
 
 private:
+	const double m_w1;
+	const double m_w2;
 	double m_dFitness;
 };
 
