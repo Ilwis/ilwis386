dca5794fea8a5883d46cbf46a42f4db01beadac1
Bas Retsios <v.retsios@utwente.nl>
2012-05-24 09:29:14 +0000

    Also use linear filter on the colors (instead of nearest neighbour) when the space time path is zoomed out
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@1595 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/SpaceTimeCube/SpaceTimePathDrawer.cpp
+++ Sources/SpaceTimeCube/SpaceTimePathDrawer.cpp
@@ -289,7 +289,7 @@ bool SpaceTimePathDrawer::draw( const CoordBounds& cbArea) const {
 		glGenTextures(1, texture);
 		glBindTexture(GL_TEXTURE_2D, *texture);
 		glTexEnvf( GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE );
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
