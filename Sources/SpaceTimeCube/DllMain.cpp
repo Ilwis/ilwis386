f03c6eb474a2c2e22b6960da407ede15d16fea27
Bas Retsios <v.retsios@utwente.nl>
2014-12-01 14:14:00 +0000

    Added 3D rose diagram drawer
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@2506 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/SpaceTimeCube/DllMain.cpp
+++ Sources/SpaceTimeCube/DllMain.cpp
@@ -2,6 +2,8 @@
 #include "client\formelements\fldlist.h"
 #include "Client\FormElements\fldcol.h"
 #include "Engine\Base\System\Module.h"
+#include "Engine\Applications\ModuleMap.h"
+#include "Engine\Base\System\commandhandler.h"
 #include "Engine\Drawers\ComplexDrawer.h"
 #include "Engine\Drawers\SimpleDrawer.h" 
 
@@ -18,6 +20,7 @@
 #include "SpaceTimePathTool.h"
 #include "SpaceTimePathDrawer.h"
 #include "StationsDrawer.h"
+#include "SpaceTimeRoseDrawer.h"
 #include "CubeDrawer.h"
 #include "PreTimeOffsetDrawer.h"
 #include "PostTimeOffsetDrawer.h"
@@ -45,6 +48,7 @@ DrawerInfoVector *createDrawer() {
 	DrawerInfoVector *infos = new DrawerInfoVector();
 	infos->push_back(new DrawerInfo("PointLayerDrawer","Cube:<stp>",createSpaceTimePathDrawer));
 	infos->push_back(new DrawerInfo("PointLayerDrawer","Cube:<stations>",createStationsDrawer));
+	infos->push_back(new DrawerInfo("PointLayerDrawer","Cube:<rose>",createSpaceTimeRoseDrawer));
 	infos->push_back(new DrawerInfo("CubeDrawer","Cube",createCubeDrawer));
 	infos->push_back(new DrawerInfo("PreTimeOffsetDrawer","Cube",createPreTimeOffsetDrawer));
 	infos->push_back(new DrawerInfo("PostTimeOffsetDrawer","Cube",createPostTimeOffsetDrawer));
@@ -52,9 +56,14 @@ DrawerInfoVector *createDrawer() {
 	return infos;
 }
 
+void moduleInit(ILWIS::Module *module) {
+	getEngine()->getContext()->ComHandler()->AddCommand("stccommand", executeStcCommand);
+}
+
 extern "C" _export ILWIS::Module *getModuleInfo(const FileName& fnModule) {
 	ILWIS::Module *module = new ILWIS::Module("SpaceTimeCube", fnModule,ILWIS::Module::mi38,"1.3");
 	module->addMethod(ILWIS::Module::ifDrawerTools, (void *)createDrawerTool);
-	module->addMethod(ILWIS::Module::ifDrawers, (void *)createDrawer);  
+	module->addMethod(ILWIS::Module::ifDrawers, (void *)createDrawer);
+	module->addMethod(ILWIS::Module::ifInit, (void *)moduleInit);
 	return module;
 }
