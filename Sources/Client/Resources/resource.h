f8b871370a1df0d79659d8b54b0ebdbd210e1a58
Bas Retsios <v.retsios@utwente.nl>
2019-09-18 11:41:39 +0000

    Removed expiry date constants (remnants)
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@3095 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/Client/Resources/resource.h
+++ Sources/Client/Resources/resource.h
@@ -2,12 +2,9 @@
 // Microsoft Visual C++ generated include file.
 // Used by ilwis30.rc
 //
-#define EXP_DATE_MONTH                  10
-#define EXP_DATE_DAY                    31
 #define IDI_ICON1                       101
 #define IDI_ICON2                       105
 #define IDB_BITMAP1                     106
-#define EXP_DATE_YEAR                   2020
 
 // Next default values for new objects
 // 
