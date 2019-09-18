18bc9562071403cafa261228bb9ee4f7c9ce95ea
Bas Retsios <v.retsios@utwente.nl>
2019-09-18 11:43:35 +0000

    Version 3.8.6 Bitmap: digit "6": font Arial size 26
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@3096 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/Headers/version.h
+++ Sources/Headers/version.h
@@ -3,14 +3,11 @@
 // file which need recent version info and version dependent strings
 
 #define VERSION_STRING "ILWIS Open"
-#define VERSION_FILEVERSIONNR 3, 8, 0, 0
-#define VERSION_PRODUCTVERSIONNR 3, 8, 0, 0
-#define VERSION_FILEVERSION "3, 8, 0, 0\0"
-#define VERSION_PRODUCTVERSION "3, 8, 0, 0\0"
+#define VERSION_FILEVERSIONNR 3, 8, 6, 0
+#define VERSION_PRODUCTVERSIONNR 3, 8, 6, 0
+#define VERSION_FILEVERSION "3, 8, 6, 0\0"
+#define VERSION_PRODUCTVERSION "3, 8, 6, 0\0"
 #define VERSION_PRIVATEBUILD "\0"
 #define VERSION_SPECIALBUILD ""
 #define ILWIS_VERSION_NAME "Ilwis 38"
 // #define BETA_VERSION
-#define EXP_DATE_YEAR 2030
-#define EXP_DATE_MONTH 10
-#define EXP_DATE_DAY 31
