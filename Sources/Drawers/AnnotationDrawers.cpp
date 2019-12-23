648ec93b10a26839e3024dd2465706d9e2a8bd8b
Bas Retsios <v.retsios@utwente.nl>
2019-12-23 15:23:58 +0000

    Point symbol scaling changed; north-arrow must be doubled in size to have the same scale as before.
    git-svn-id: https://svn.52north.org/svn/ilwis/drawers@3230 b64c5c0c-1e74-440b-9268-4ad09cf26390

--- Sources/Drawers/AnnotationDrawers.cpp
+++ Sources/Drawers/AnnotationDrawers.cpp
@@ -1854,7 +1854,7 @@ AnnotationNorthArrowDrawer::AnnotationNorthArrowDrawer(DrawerParameters *parms)
 		cb.MaxY() = cbMap.MaxY();
 	begin.y = 0.9;
 	begin.x = 0.9;
-	scale = 2.5;
+	scale = 5.0;
 }
 
 AnnotationNorthArrowDrawer::~AnnotationNorthArrowDrawer()
